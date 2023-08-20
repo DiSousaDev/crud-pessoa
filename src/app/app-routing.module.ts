2fd0e0fe788a0c2872b4d57ec4d38dddbcb1ff4d
Eder Diego de Sousa <me@diego.dev.br>
2023-08-20 02:29:23 -0300

    Listar Cidades

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { EditarPessoaComponent } from "./pessoa/editar-pessoa/editar-pessoa.comp
 import { ListarEnderecoComponent } from "./endereco/listar-endereco/listar-endereco.component";
 import { InserirEnderecoComponent } from "./endereco/inserir-endereco/inserir-endereco.component";
 import { EditarEnderecoComponent } from "./endereco/editar-endereco/editar-endereco.component";
+import { ListarCidadeComponent } from "./cidade/listar-cidade/listar-cidade.component";
 
 const routes: Routes = [
   {
@@ -41,6 +42,10 @@ const routes: Routes = [
   {
     path: 'enderecos/editar/:id',
     component: EditarEnderecoComponent
+  },
+  {
+    path: 'cidades/listar',
+    component: ListarCidadeComponent
   }
 ];
 
--- src/app/cidade/cidade.module.ts
+++ src/app/cidade/cidade.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CidadeService } from "./services/cidade.service";
+import { ListarCidadeComponent } from './listar-cidade/listar-cidade.component';
+import { RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListarCidadeComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule
   ],
   providers: [
     CidadeService
